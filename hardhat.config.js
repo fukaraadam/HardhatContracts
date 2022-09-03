29cd9f76bdf01426e3f4c699693c4f5de1c585ce
Furkan ALAN <furkanalan@hotmail.com>
2022-09-04 01:08:59 +0300

    dotenv installed. Contract deployed to Rinkeby test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-truffle5");
+require('dotenv').config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_RINKEBY_API_KEY;
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const MAINACCOUNT_PRIVATE_KEY = process.env.MAINACCOUNT_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MAINACCOUNT_PRIVATE_KEY]
+    }
+  }
 };
